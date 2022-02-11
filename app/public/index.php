b4a023907b83991058f84d872eb02509e7b553d6
mahdi <ghasemim875@gmail.com>
2022-02-11 10:38:17 +0330

    add env and logs

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,14 @@
 <?php
+$app_name = $_ENV['APP_NAME'] ?? "-";
 
-echo "Hello \n";
+echo "<h1> Test Done </h1>";
+echo "<h2> Welcome to {$app_name} app</h2>";
+
+echo '<h3><pre>';
+$time = date("Y.d.m G:i:s");
+$agent = $_SERVER['HTTP_USER_AGENT'];
+exec('whoami', $output);
+$user = $output[0];
+$log = "{$time} exec by {$user} : {$agent} \n";
+
+file_put_contents('../logs/request.log', $log, FILE_APPEND);
