70715c814a9e3abb1efb1c22918e7baf9fc90c3a
mahdi <ghasemim875@gmail.com>
2022-02-13 19:05:22 +0330

    connect to db

--- app/public/index.php
+++ app/public/index.php
@@ -1,14 +1,24 @@
 <?php
-$app_name = $_ENV['APP_NAME'] ?? "-";
+include_once '../helpers/Env.php';
+include_once '../helpers/Db.php';
+include_once '../helpers/Log.php';
+
+$logger = new Log;
+$db = new Db;
+
+$app_name = Env::get('APP_NAME');
+
+
 
 echo "<h1> Test Done </h1>";
 echo "<h2> Welcome to {$app_name} app</h2>";
 
-echo '<h3><pre>';
-$time = date("Y.d.m G:i:s");
-$agent = $_SERVER['HTTP_USER_AGENT'];
-exec('whoami', $output);
-$user = $output[0];
-$log = "{$time} exec by {$user} : {$agent} \n";
+$db->migrate();
+$db->insert('title 1', 'body 1 bomo tepo');
+$posts = $db->getAll();
 
-file_put_contents('../logs/request.log', $log, FILE_APPEND);
+echo '<pre>';
+print_r($posts);
+echo '</pre>';
+
+$logger::write('Test DOPE');
